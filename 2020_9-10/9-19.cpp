e40183fb5d4b1cf00d0a53347845c8d9bd6365fd
华华华华 <1055928999@qq.com>
2020-09-19 11:27:09 +0800

    404. 左叶子之和

--- 2020_9-10/9-19.cpp
+++ 2020_9-10/9-19.cpp
@@ -1,14 +1,23 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
+#include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-class Solution {
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution
+{
 public:
-    int sumOfLeftLeaves(TreeNode* root) {
-        if(root == NULL) return 0;
-        return sumOfLeftLeaves(root->left) 
-            + sumOfLeftLeaves(root->right) 
-            + (root->left && !root->left->left && !root->left->right ? root->left->val : 0);
+    int sumOfLeftLeaves(TreeNode *root)
+    {
+        if (root == NULL)
+            return 0;
+        return sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right) + (root->left && !root->left->left && !root->left->right ? root->left->val : 0);
     }
 };
