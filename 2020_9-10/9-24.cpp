d01dd83f4956f150ab7b2029cc6304012015e407
华华华华 <1055928999@qq.com>
2020-09-24 20:57:30 +0800

    501. 二叉搜索树中的众数

---
+++ 2020_9-10/9-24.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int base, count, maxCount;
+    vector<int> answer;
+
+    void update(int x) {
+        if (x == base) {
+            ++count;
+        } else {
+            count = 1;
+            base = x;
+        }
+        if (count == maxCount) {
+            answer.push_back(base);
+        }
+        if (count > maxCount) {
+            maxCount = count;
+            answer = vector<int> {base};
+        }
+    }
+
+    vector<int> findMode(TreeNode* root) {
+        TreeNode *cur = root, *pre = nullptr;
+        while (cur) {
+            if (!cur->left) {
+                update(cur->val);
+                cur = cur->right;
+                continue;
+            }
+            pre = cur->left;
+            while (pre->right && pre->right != cur) {
+                pre = pre->right;
+            }
+            if (!pre->right) {
+                pre->right = cur;
+                cur = cur->left;
+            } else {
+                pre->right = nullptr;
+                update(cur->val);
+                cur = cur->right;
+            }
+        }
+        return answer;
+    }
+};
